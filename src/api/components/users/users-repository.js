43498be0b57d18e7a83e606795124e46f06c4f98
irenecarll <irene.carllewisgmail.com>
2024-05-04 08:05:04 +0700

    first commit

---
+++ src/api/components/users/users-repository.js
@@ -0,0 +1,124 @@
+const { User } = require('../../../models');
+
+/**
+ * Get a list of users
+ * @returns {Promise}
+ */
+async function getUsers() {
+  return User.find({});
+}
+
+/**
+ * Get user detail
+ * @param {string} id - User ID
+ * @returns {Promise}
+ */
+async function getUser(id) {
+  return User.findById(id);
+}
+
+/**
+ * Count total users based on search criteria
+ * @param {string} search - Search keyword for filtering
+ * @returns {Promise<number>} - Total count of users
+ */
+async function countUsers(search) {
+  try {
+    let query = User.find();
+
+    // filtering based on search keyword
+    if (search) {
+      query = query.where('email').regex(new RegExp(search, 'i'));
+    }
+
+    // count total users
+    const totalUsers = await User.countDocuments(query);
+    return totalUsers;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * Get user detail
+ * @param {string} id - User ID
+ * @returns {Promise}
+ */
+async function getUser(id) {
+  return User.findById(id);
+}
+
+/**
+ * Create new user
+ * @param {string} name - Name
+ * @param {string} email - Email
+ * @param {string} password - Hashed password
+ * @returns {Promise}
+ */
+async function createUser(name, email, password) {
+  return User.create({
+    name,
+    email,
+    password,
+  });
+}
+
+/**
+ * Update existing user
+ * @param {string} id - User ID
+ * @param {string} name - Name
+ * @param {string} email - Email
+ * @returns {Promise}
+ */
+async function updateUser(id, name, email) {
+  return User.updateOne(
+    {
+      _id: id,
+    },
+    {
+      $set: {
+        name,
+        email,
+      },
+    }
+  );
+}
+
+/**
+ * Delete a user
+ * @param {string} id - User ID
+ * @returns {Promise}
+ */
+async function deleteUser(id) {
+  return User.deleteOne({ _id: id });
+}
+
+/**
+ * Get user by email to prevent duplicate email
+ * @param {string} email - Email
+ * @returns {Promise}
+ */
+async function getUserByEmail(email) {
+  return User.findOne({ email });
+}
+
+/**
+ * Update user password
+ * @param {string} id - User ID
+ * @param {string} password - New hashed password
+ * @returns {Promise}
+ */
+async function changePassword(id, password) {
+  return User.updateOne({ _id: id }, { $set: { password } });
+}
+
+module.exports = {
+  getUsers,
+  countUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  getUserByEmail,
+  changePassword,
+};
