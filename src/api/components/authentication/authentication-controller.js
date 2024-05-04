ec377dbaf52d82daee065740d3794c72577a7e22
irenecarll <irene.carllewisgmail.com>
2024-05-04 23:17:49 +0700

    bikin attempt di message

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -2,7 +2,7 @@ const { errorResponder, errorTypes } = require('../../../core/errors');
 const authenticationServices = require('./authentication-service');
 
 let loginAttempts = {};
-const LOGIN_ATTEMPT_TIME_RESET = 30 * 60 * 1000; // set waktu 30 menit untuk reset attempt menjadi 0
+const LOGIN_ATTEMPT_TIME_RESET = 30 * 60 * 1000; // set wakt 30 menit untuk reset attempt jadi 0
 /**
  * Handle login request
  * @param {object} request - Express request object
@@ -14,10 +14,10 @@ async function login(request, response, next) {
   const { email, password } = request.body;
 
   try {
-    const recent = Date.now();  // waktu sekarang
+    const recent = Date.now(); // waktu sekarang yg akan di tampilkan dalam output api
     const lastAttempt = loginAttempts[email];
 
-    
+    let attempt;
 
     if (lastAttempt && recent - lastAttempt.time < LOGIN_ATTEMPT_TIME_RESET){
       if (lastAttempt.count > 5) {
@@ -43,13 +43,13 @@ async function login(request, response, next) {
       if (loginAttempts[email].count > 5){
         attempt = loginAttempts[email].count;
         throw errorResponder(
-          errorTypes.FORBIDDEN,`[${new Date().toISOString()}] User ${email} login gagal. telah mencapai limit, coba lagi setelah 30 menit.`
+          errorTypes.FORBIDDEN,`[${new Date().toISOString()}] User ${email} login gagal. Attempt= ${attempts}. telah mencapai limit, coba lagi setelah 30 menit.`
         );
         
       } else {
         attempt = loginAttempts[email].count;
         throw errorResponder(
-          errorTypes.INVALID_CREDENTIALS,`[${new Date().toISOString()}] User ${email} login gagal. `
+          errorTypes.INVALID_CREDENTIALS,`[${new Date().toISOString()}] User ${email} login gagal. Attempt= ${attempt}.`
         );
       }
       
